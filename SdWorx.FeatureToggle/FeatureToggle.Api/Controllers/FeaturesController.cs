905603b70074c343e72a6016b3d4a107c1c659ad
Matthew Quinn <matthew.quinn@sdworx.com>
2023-04-12 10:35:25 +0100

    Prefix 'api' to controller routes as to avoid WAF conflicts.

--- SdWorx.FeatureToggle/FeatureToggle.Api/Controllers/FeaturesController.cs
+++ SdWorx.FeatureToggle/FeatureToggle.Api/Controllers/FeaturesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace FeatureToggle.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class FeaturesController : ControllerBase
 {
     private ISender _mediator = null!;
