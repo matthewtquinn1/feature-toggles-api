18e7d7a9a4e962e3c36dd6b4eb890e638ee5a799
Matthew Quinn <matthew.quinn@sdworx.com>
2023-04-19 11:17:06 +0100

    Remove ability to edit name. Too risky a feature to expose. Create new easier.

--- SdWorx.FeatureToggle/FeatureToggle.Application/Features/Commands/UpdateFeatureCommand.cs
+++ SdWorx.FeatureToggle/FeatureToggle.Application/Features/Commands/UpdateFeatureCommand.cs
@@ -1,6 +1,5 @@
 ﻿using FeatureToggle.Application.Common.Exceptions;
 using FeatureToggle.Application.Common.Interfaces;
-using FeatureToggle.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,7 +7,6 @@ namespace FeatureToggle.Application.Features.Commands;
 
 public sealed record UpdateFeatureCommand(
     Guid Id,
-    string Name,
     string Description) : IRequest<FeatureDto>;
 
 public sealed class UpdateFeatureCommandHandler : IRequestHandler<UpdateFeatureCommand, FeatureDto>
@@ -32,16 +30,6 @@ public sealed class UpdateFeatureCommandHandler : IRequestHandler<UpdateFeatureC
             throw new NotFoundException(nameof(feature), request.Id);
         }
 
-        var existingFeature = await _context.Features
-            .AsNoTracking()
-            .FirstOrDefaultAsync(f => f.Name == request.Name && f.Id != request.Id, cancellationToken);
-
-        if (existingFeature != null)
-        {
-            throw new DuplicateFoundException(nameof(Feature), nameof(request.Name), request.Name);
-        }
-
-        feature.Name = request.Name;
         feature.Description = request.Description;
 
         _ = _context.Features.Update(feature);
